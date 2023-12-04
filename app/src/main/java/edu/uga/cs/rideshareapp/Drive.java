159815000b6a3a46b87590443d266b916ba4041d
BoLewis33793 <105173646+BoLewis33793@users.noreply.github.com>
2023-12-04 17:07:33 -0500

    Added rider and driver class.

--- app/src/main/java/edu/uga/cs/rideshareapp/Drive.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/Drive.java
@@ -7,4 +7,61 @@ public class Drive {
     public Drive() {
 
     }
+
+    public Drive(String pickup_location, String destination, String driver, String date, String time, int points) {
+        this.pickup_location = pickup_location;
+        this.destination = destination;
+        this.driver = driver;
+        this.date = date;
+        this.time = time;
+        this.points = points;
+    }
+
+    public String getPickup_location() {
+        return pickup_location;
+    }
+
+    public void setPickup_location(String pickup_location) {
+        this.pickup_location = pickup_location;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
--- app/src/main/java/edu/uga/cs/rideshareapp/Ride.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/Ride.java
@@ -1,10 +1,67 @@
 package edu.uga.cs.rideshareapp;
 
 public class Ride {
-    String location;
+    String pickup_location, destination, rider, date, time;
     int points;
 
     public Ride() {
 
+    }
+
+    public Ride(String pickup_location, String destination, String rider, String date, String time, int points) {
+        this.pickup_location = pickup_location;
+        this.destination = destination;
+        this.rider = rider;
+        this.date = date;
+        this.time = time;
+        this.points = points;
+    }
+
+    public String getPickup_location() {
+        return pickup_location;
+    }
+
+    public void setPickup_location(String pickup_location) {
+        this.pickup_location = pickup_location;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDriver() {
+        return rider;
+    }
+
+    public void setDriver(String driver) {
+        this.rider = driver;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
     }
 }
