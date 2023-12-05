1b94c72b38920710b678bf871a8fa650c0d80da2
nik <nikmannik01@gmail.com>
2023-12-05 15:55:49 -0500

    working menu - only implemented in HomePage.java so far

--- app/src/main/java/edu/uga/cs/rideshareapp/HomePage.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/HomePage.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,6 +33,9 @@ public class HomePage extends AppCompatActivity {
         Button createDrive = findViewById(R.id.button5);
         Button allRides = findViewById(R.id.button6);
         Button allDrives = findViewById(R.id.button7);
+        ImageView menuIcon = findViewById(R.id.menu_icon);
+        menuIcon.setOnClickListener(view -> showPopupMenu(view));
+        
 
         textView.setText(userEmail);
 
@@ -70,4 +75,23 @@ public class HomePage extends AppCompatActivity {
             }
         });
     }
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.setOnMenuItemClickListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_all_rides) {
+                // Intent to start Activity that shows all rides
+                startActivity(new Intent(this, AllRidesList.class));
+                return true;
+            } else if (itemId == R.id.action_view_profile) {
+                // Intent to start Activity that shows profile
+                startActivity(new Intent(this, ProfileMenu.class));
+                return true;
+            }
+            return false;
+        });
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
 }
